4882f0817fb4dbb23a16d4e04350545294b1ce92
oranzhang <423794590@qq.com>
2014-02-13 14:44:27 +0800

    ...

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,6 +1,6 @@
 var disqus_shortname = 'oranzhang';
 var hostbase = 'http://oran.pw';
 var githubname = 'oranzhang';
-var repos = 'Hooloo';
+var repos = 'oranzhang.github.io';
 var sitetitle = 'Oran Zhang';
 var rss = '';
