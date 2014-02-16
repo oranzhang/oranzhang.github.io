3d5a23ffb94a2f62e7abf598e9bd8c0cbcd32cda
Oran Zhang <mr.q.zjc2006@gmail.com>
2014-02-16 16:33:41 +0800

    ...

---
+++ javascripts/config.js
@@ -0,0 +1,6 @@
+var disqus_shortname = 'oranzhang';
+var hostbase = 'http://oran.pw';
+var githubname = 'oranzhang';
+var repos = 'oranzhang.github.io';
+var sitetitle = 'Oran Zhang';
+var rss = '';
